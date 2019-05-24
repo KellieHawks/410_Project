afdcd2a1681cde7e61072140a02337d38c56312a
Kellie Hawks <kelliehawks96@gmail.com>
2019-05-23 18:54:34 -0700

    arrow points to endpoint. need it to point to cat.
    added arrow.

---
+++ 410_Project/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ArrowController : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
