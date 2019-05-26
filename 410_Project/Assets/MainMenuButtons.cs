4e1c054908c683853053c017e218641be26edf76
Kellie Hawks <kelliehawks96@gmail.com>
2019-05-26 15:24:09 -0700

    main menu
    added a main menu

---
+++ 410_Project/Assets/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
